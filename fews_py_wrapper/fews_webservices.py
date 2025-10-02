bc7c36ccfd7ac3008c4074c165e64d601d0b2e69
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-10-02 11:53:32 +0200

    added outline

---
+++ fews_py_wrapper/fews_webservices.py
@@ -0,0 +1,18 @@
+import xarray as xr
+
+
+def get_timeseries(*args, **kwargs) -> xr.Dataset:
+    """Get time series data from the FEWS web services."""
+    pass
+
+def get_taskrun_status(*args, **kwargs) -> dict:
+    """Get the status of a task run in the FEWS web services."""
+    pass
+
+def execute_workflow(*args, **kwargs):
+    """Execute a workflow in the FEWS web services."""
+    pass
+
+def execute_whatif_scenario(*args, **kwargs):
+    """Execute a what-if scenario in the FEWS web services."""
+    pass
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from fews-py-wrapper!")
-
-
-if __name__ == "__main__":
-    main()
