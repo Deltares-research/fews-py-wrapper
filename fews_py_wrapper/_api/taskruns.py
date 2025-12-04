f61edd346f984474978d40eb9a6f8e1a6910e9f0
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-12-04 11:39:51 +0100

    added testing for retrieve_taskruns call

--- fews_py_wrapper/_api/taskruns.py
+++ fews_py_wrapper/_api/taskruns.py
@@ -1,3 +1,5 @@
+import json
+
 from fews_openapi_py_client import AuthenticatedClient, Client
 from fews_openapi_py_client.api.tasks import taskruns
 from fews_openapi_py_client.models.taskruns_document_format import (
@@ -18,8 +20,9 @@ def retrieve_taskruns(
     response = taskruns.sync_detailed(
         client=client, workflow_id=workflow_id, task_ids=task_ids, **kwargs
     )
-    response.raise_for_status()
-    return response.parsed
+    if response.status_code != 200:
+        response.raise_for_status()
+    return json.loads(response.content.decode("utf-8"))
 
 
 def retrieve_argument_models(kwargs: dict) -> dict:
@@ -30,9 +33,19 @@ def retrieve_argument_models(kwargs: dict) -> dict:
                 kwargs["document_format"]
             )
         if "only_current" in kwargs:
-            kwargs["only_current"] = TaskrunsOnlyCurrent(kwargs["only_current"])
+            kwargs["only_current"] = TaskrunsOnlyCurrent(
+                convert_bools(kwargs["only_current"])
+            )
         if "only_forecasts" in kwargs:
-            kwargs["only_forecasts"] = TaskrunsOnlyForecasts(kwargs["only_forecasts"])
+            kwargs["only_forecasts"] = TaskrunsOnlyForecasts(
+                convert_bools(kwargs["only_forecasts"])
+            )
         return kwargs
     except ValueError as e:
         raise ValueError(f"Invalid argument value: {e}") from e
+
+
+def convert_bools(arg):
+    if arg:
+        return "true"
+    return "false"
--- tests/test_api/test_taskruns.py
+++ tests/test_api/test_taskruns.py
@@ -1,10 +1,47 @@
+import json
+from unittest.mock import Mock
+
+import httpx
+import pytest
+import requests
 from fews_openapi_py_client.models.taskruns_document_format import (
     TaskrunsDocumentFormat,
 )
 from fews_openapi_py_client.models.taskruns_only_current import TaskrunsOnlyCurrent
 from fews_openapi_py_client.models.taskruns_only_forecasts import TaskrunsOnlyForecasts
 
-from fews_py_wrapper._api.taskruns import retrieve_argument_models
+from fews_py_wrapper._api.taskruns import retrieve_argument_models, retrieve_taskruns
+
+
+def test_retrieve_taskruns(mocker):
+    workflow_id = "test_workflow_id"
+    task_ids = ["test_task_id"]
+    document_format = "PI_JSON"
+    mock_response = httpx.Response(
+        status_code=200,
+        content=json.dumps({"taskruns": []}).encode(),
+        headers={"content-type": "application/json"},
+    )
+    mocker.patch(
+        "fews_py_wrapper._api.taskruns.taskruns.sync_detailed",
+        return_value=mock_response,
+    )
+    response = retrieve_taskruns(
+        client=Mock(),
+        workflow_id=workflow_id,
+        task_ids=task_ids,
+        document_format=document_format,
+    )
+    assert response == {"taskruns": []}
+    mock_response.status_code = 404
+    mock_response.raise_for_status = Mock(side_effect=requests.HTTPError)
+    with pytest.raises(requests.HTTPError):
+        retrieve_taskruns(
+            client=Mock(),
+            workflow_id=workflow_id,
+            task_ids=task_ids,
+            document_format=document_format,
+        )
 
 
 def test_retrieve_argument_models() -> None:
@@ -24,3 +61,7 @@ def test_retrieve_argument_models() -> None:
 
     assert isinstance(converted_kwargs["only_forecasts"], TaskrunsOnlyForecasts)
     assert converted_kwargs["only_forecasts"] == TaskrunsOnlyForecasts.FALSE
+
+    with pytest.raises(ValueError, match="Invalid argument value: "):
+        kwargs = {"document_format": "INVALID_FORMAT"}
+        retrieve_argument_models(kwargs)
